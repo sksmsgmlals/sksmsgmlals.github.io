8c01700eefd15af9f7afe9622e3ada9bbd75ebc6
sksmsgmlals <127678021+sksmsgmlals@users.noreply.github.com>
2023-10-11 22:15:15 +0900

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int n;
+    scanf("%d",&n);
+    for (int i=0;i<n;i++){
+        for(int k=n-i-1;k>0;k--)
+            printf(" ");
+        for(int k=0;k<(i+1)*2-1;k++)
+                printf("*");
+        printf("\n");
+    }
+    for (int i=0;i<n;i++){
+        for(int k=0;k<(i+1);k++)
+            printf(" ");
+        for(int k=n*2-(i+1)*2-1;k>0;k--)
+            printf("*");
+        printf("\n");
+
+
+    }
+    return 0;
+}
