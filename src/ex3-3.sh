fbebf2062a1d119c392979c78448b7fadbd1bc40
sksmsgmlals <127678021+sksmsgmlals@users.noreply.github.com>
2023-10-18 18:50:32 +0900

    Add files via upload

---
+++ src/ex3-3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+bmi=`expr $1 \* 100000 / $2 / $2`
+if [[ $bmi -ge 180 ]] && [[ $bmi -lt 230 ]]
+then
+	echo "정상체중입니다"
+elif [[ $bmi -lt  180 ]]
+then
+	echo "저체중입니다"
+elif [[ $bmi -ge 230 ]]
+then
+	echo "과체중입니다."
+
+fi
+exit 0
